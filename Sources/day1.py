b082750665e42358cc6bdcb3c5eeb80d64c26da1
lykosi <philippe.patey@protonmail.com>
2024-12-02 14:34:55 +0100

    PhPat : Day 1 answer

---
+++ Sources/day1.py
@@ -0,0 +1,25 @@
+import re
+
+result = []
+rightColumn = []
+leftColumn = []
+
+data = open('Inputs\day1.txt', 'r').read().split('\n')
+for sDistances in data:
+    iDistances = list(map(int, sDistances.split()))
+    leftColumn.append(iDistances[0])
+    rightColumn.append(iDistances[1])
+
+for index in range(len(data)):
+    iMinRightColumn = min(rightColumn)
+    rightColumn.remove(iMinRightColumn)
+    
+    iMinLeftColumn = min(leftColumn)
+    leftColumn.remove(iMinLeftColumn)
+
+    iMin = min(iMinRightColumn, iMinLeftColumn)
+    iMax = max(iMinRightColumn, iMinLeftColumn)
+    result.append(iMax - iMin)
+
+response = sum(result)
+print("The total distance between the lists is:", response) #2430334
