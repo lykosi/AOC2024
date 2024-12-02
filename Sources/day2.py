647deeb43ef9cccc0209bb5bee5883fbaa291a4d
lykosi <philippe.patey@protonmail.com>
2024-12-02 14:46:57 +0100

    PhPat : Day 2 answer

---
+++ Sources/day2.py
@@ -0,0 +1,18 @@
+import re
+
+result = []
+rightColumn = []
+leftColumn = []
+
+data = open('Inputs\day1.txt', 'r').read().split('\n')
+for sDistances in data:
+    iDistances = list(map(int, sDistances.split()))
+    leftColumn.append(iDistances[0])
+    rightColumn.append(iDistances[1])
+
+for value in leftColumn:
+    occurrence = rightColumn.count(value)
+    result.append(value * occurrence)
+
+response = sum(result)
+print("The similarity score is:", response) #28786472
